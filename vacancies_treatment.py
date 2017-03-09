2b1646b7f5abdb34b3e13a37efa4113c100434d0
voron434 <070809010@mail.ru>
2017-03-10 01:54:48 +0300

    making functions cleaner

--- vacancies_treatment.py
+++ vacancies_treatment.py
@@ -1,4 +1,5 @@
 import json
+import os
 
 def get_vacancy_params(vacancy):
     parameters = {
@@ -9,23 +10,17 @@ def get_vacancy_params(vacancy):
         }
     return parameters
 
-def load_data():
-    print('Enter path to DataBase:')
-    path = input()
-    try:
-        with open(path, mode='r', encoding='utf-8') as my_file:
-            data = json.load(my_file)
-            return data
-    except FileNotFoundError:
+if __name__ == '__main__':
+    input_path = input('Enter path to DataBase:')
+    if not os.path.exists(input_path):
         print('File not found, sorry...')
         raise SystemExit
-
-if __name__ == '__main__':
-    data = load_data()
+    with open(input_path, mode='r', encoding='utf-8') as my_file:
+        data = json.load(my_file)
     simple_vacancies = []
     for vacancy in data['objects']:
         parameters = get_vacancy_params(vacancy)
         simple_vacancies.append(parameters)
-    path = 'simple_vacancies.json'
-    with open(path, mode='w', encoding='utf-8') as my_file:
-        json.dump(simple_vacancies, my_file)
+    output_path = 'simple_vacancies.json'
+    with open(output_path, mode='w', encoding='utf-8') as my_new_file:
+        json.dump(simple_vacancies, my_new_file)
